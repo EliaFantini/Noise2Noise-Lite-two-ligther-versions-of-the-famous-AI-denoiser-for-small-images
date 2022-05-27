5e44f71d37fb8441d89c3fc18da8d7a96ec634e6
Elia Fantini <62103572+EliaFantini@users.noreply.github.com>
2022-05-27 09:53:07 +0200

    Add files via upload

--- Miniproject_1/others/nets/unet.py
+++ Miniproject_1/others/nets/unet.py
@@ -3,14 +3,12 @@ import torch.nn as nn
 
 
 class UNet(nn.Module):
-    """Custom U-Net architecture for Noise2Noise (see Appendix, Table 2)."""
+    """Custom U-Net architecture"""
 
     def __init__(self, in_channels=3, out_channels=3):
-        """Initializes U-Net."""
-
         super(UNet, self).__init__()
         depthChannels= 24
-        # Layers: enc_conv0, enc_conv1, pool1
+
         self._block1 = nn.Sequential(
             nn.Conv2d(in_channels, depthChannels, 3, stride=1, padding=1),
             nn.ReLU(inplace=True),
@@ -18,39 +16,33 @@ class UNet(nn.Module):
             nn.ReLU(inplace=True),
             nn.MaxPool2d(2))
 
-        # Layers: enc_conv(i), pool(i); i=2..5
+
         self._block2 = nn.Sequential(
             nn.Conv2d(depthChannels, depthChannels, 3, stride=1, padding=1),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(2))
 
 
-        # Layers: enc_conv6, upsample5
+
         self._block3 = nn.Sequential(
             nn.Conv2d(depthChannels, depthChannels, 3, stride=1, padding=1),
             nn.ReLU(inplace=True),
             nn.ConvTranspose2d(depthChannels, depthChannels, 3, stride=2, padding=1, output_padding=1))
-            #nn.Upsample(scale_factor=2, mode='nearest'))
 
-        # Layers: dec_conv5a, dec_conv5b, upsample4
         self._block4 = nn.Sequential(
             nn.Conv2d(depthChannels*2, depthChannels*2, 3, stride=1, padding=1),
             nn.ReLU(inplace=True),
             nn.Conv2d(depthChannels*2, depthChannels*2, 3, stride=1, padding=1),
             nn.ReLU(inplace=True),
             nn.ConvTranspose2d(depthChannels*2, depthChannels*2, 3, stride=2, padding=1, output_padding=1))
-            #nn.Upsample(scale_factor=2, mode='nearest'))
 
-        # Layers: dec_deconv(i)a, dec_deconv(i)b, upsample(i-1); i=4..2
         self._block5 = nn.Sequential(
             nn.Conv2d(depthChannels*3, depthChannels*2, 3, stride=1, padding=1),
             nn.ReLU(inplace=True),
             nn.Conv2d(depthChannels*2, depthChannels*2, 3, stride=1, padding=1),
             nn.ReLU(inplace=True),
             nn.ConvTranspose2d(depthChannels*2, depthChannels*2, 3, stride=2, padding=1, output_padding=1))
-            #nn.Upsample(scale_factor=2, mode='nearest'))
 
-        # Layers: dec_conv1a, dec_conv1b, dec_conv1c,
         self._block6 = nn.Sequential(
             nn.Conv2d(depthChannels*2 + in_channels, depthChannels*2, 3, stride=1, padding=1),
             nn.ReLU(inplace=True),
@@ -59,9 +51,7 @@ class UNet(nn.Module):
             nn.Conv2d(32, out_channels, 3, stride=1, padding=1),
             nn.LeakyReLU(0.1))
 
-        # Initialize weights
         self._init_weights()
-
 
     def _init_weights(self):
         """Initializes weights using He et al. (2015)."""
@@ -71,10 +61,7 @@ class UNet(nn.Module):
                 nn.init.kaiming_normal_(m.weight.data)
                 m.bias.data.zero_()
 
-
     def forward(self, x):
-        """Through encoder, then decoder by adding U-skip connections. """
-
         # Encoder
         pool1 = self._block1(x)
         pool2 = self._block2(pool1)
@@ -94,5 +81,4 @@ class UNet(nn.Module):
         upsample1 = self._block5(concat2)
         concat1 = torch.cat((upsample1, x), dim=1)
 
-        # Final activation
         return self._block6(concat1)
--- Miniproject_1/others/nets/unet2.py
+++ Miniproject_1/others/nets/unet2.py
@@ -3,14 +3,12 @@ import torch.nn as nn
 
 
 class UNet(nn.Module):
-    """Custom U-Net architecture for Noise2Noise (see Appendix, Table 2)."""
+    """Original U-Net architecture from Noise2Noise """
 
     def __init__(self, in_channels=3, out_channels=3):
-        """Initializes U-Net."""
-
         super(UNet, self).__init__()
         depthChannels= 48
-        # Layers: enc_conv0, enc_conv1, pool1
+
         self._block1 = nn.Sequential(
             nn.Conv2d(in_channels, depthChannels, 3, stride=1, padding=1),
             #nn.BatchNorm2d(depthChannels),
@@ -20,7 +18,6 @@ class UNet(nn.Module):
             nn.LeakyReLU(inplace=True),
             nn.MaxPool2d(2))
 
-        # Layers: enc_conv(i), pool(i); i=2..5
         self._block2 = nn.Sequential(
             nn.Conv2d(depthChannels, depthChannels, 3, stride=1, padding=1),
             #nn.BatchNorm2d(depthChannels),
@@ -42,16 +39,12 @@ class UNet(nn.Module):
             nn.LeakyReLU(inplace=True),
             nn.MaxPool2d(2))
 
-
-        # Layers: enc_conv6, upsample5
         self._block6 = nn.Sequential(
             nn.Conv2d(depthChannels, depthChannels, 3, stride=1, padding=1),
             #nn.BatchNorm2d(depthChannels),
             nn.LeakyReLU(inplace=True),
             nn.ConvTranspose2d(depthChannels, depthChannels, 3, stride=2, padding=1, output_padding=1))
-            #nn.Upsample(scale_factor=2, mode='nearest'))
 
-        # Layers: dec_conv5a, dec_conv5b, upsample4
         self._block7 = nn.Sequential(
             nn.Conv2d(depthChannels*2, depthChannels*2, 3, stride=1, padding=1),
             #nn.BatchNorm2d(depthChannels*2),
@@ -60,9 +53,7 @@ class UNet(nn.Module):
             #nn.BatchNorm2d(depthChannels * 2),
             nn.LeakyReLU(inplace=True),
             nn.ConvTranspose2d(depthChannels*2, depthChannels*2, 3, stride=2, padding=1, output_padding=1))
-            #nn.Upsample(scale_factor=2, mode='nearest'))
 
-        # Layers: dec_deconv(i)a, dec_deconv(i)b, upsample(i-1); i=4..2
         self._block8 = nn.Sequential(
             nn.Conv2d(depthChannels*3, depthChannels*2, 3, stride=1, padding=1),
             #nn.BatchNorm2d(depthChannels * 2),
@@ -89,9 +80,7 @@ class UNet(nn.Module):
             #nn.BatchNorm2d(depthChannels * 2),
             nn.LeakyReLU(inplace=True),
             nn.ConvTranspose2d(depthChannels*2, depthChannels*2, 3, stride=2, padding=1, output_padding=1))
-            #nn.Upsample(scale_factor=2, mode='nearest'))
 
-        # Layers: dec_conv1a, dec_conv1b, dec_conv1c,
         self._block11 = nn.Sequential(
             nn.Conv2d(depthChannels*2 + in_channels, depthChannels*2, 3, stride=1, padding=1),
             #nn.BatchNorm2d(depthChannels * 2),
@@ -102,9 +91,7 @@ class UNet(nn.Module):
             nn.Conv2d(32, out_channels, 3, stride=1, padding=1),
             nn.ReLU())
 
-        # Initialize weights
         self._init_weights()
-
 
     def _init_weights(self):
         """Initializes weights using He et al. (2015)."""
@@ -114,10 +101,7 @@ class UNet(nn.Module):
                 nn.init.kaiming_normal_(m.weight.data)
                 m.bias.data.zero_()
 
-
     def forward(self, x):
-        """Through encoder, then decoder by adding U-skip connections. """
-
         # Encoder
         pool1 = self._block1(x)
         pool2 = self._block2(pool1)
@@ -137,5 +121,4 @@ class UNet(nn.Module):
         upsample1 = self._block10(concat2)
         concat1 = torch.cat((upsample1, x), dim=1)
 
-        # Final activation
         return self._block11(concat1)
